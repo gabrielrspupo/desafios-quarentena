889778cd018e45ce24555e49e9ba02f0ef0a61e8
gabrielrspupo <gabrielrspupo@gmail.com>
2020-05-07 02:16:55 -0300

    [01] Fifth bonus challenge solved

--- Desafio 1/ex.js
+++ Desafio 1/ex.js
@@ -1,28 +1,147 @@
-const playerHpElement = document.getElementById('player-health');
-let playerTotalHp = 280;
-let playerHp = 280;
+class Fighter {
+  constructor(name, type, totalHp, hpElement, attacks, weaknesses, weaknessIndex, main) {
+    this.name = name;
+    this.type = type;
+    this.stage = 0;
+    this.frozen = 0;
+    this.totalHp = totalHp;
+    this.hp = totalHp;
+    this.hpElement = hpElement;
+    this.attacks = attacks;
+    this.weaknesses = weaknesses;
+    this.weaknessIndex = weaknessIndex;
+    this.main = main;
+    this.opponent = null;
+  }
 
-const opponentHpElement = document.getElementById('opponent-health');
-let opponentTotalHp = 380;
-let opponentHp = 380;
+  set name(newName) {
+    this._name = newName;
+  }
+
+  set versus(opponent) {
+    this.opponent = opponent;
+  }
+
+  set hp(newHP) {
+    this._hp = newHP;
+  }
+
+  set hpElement(newHpElement) {
+    this._hpElement = newHpElement;
+  }
+
+  set freeze(rounds) {
+    this.frozen = rounds;
+  }
+
+  set totalHp(newHP) {
+    this._totalHp = newHP;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get versus() {
+    return this.opponent;
+  }
+
+  get hpElement() {
+    return this._hpElement;
+  }
+
+  get hp() {
+    return this._hp;
+  }
+
+  get totalHp() {
+    return this._totalHp;
+  }
+
+  get frozenRounds() {
+    return this.frozen;
+  }
+
+  updateHp(newHP) {
+    // Prevents the HP to go off boundaries
+    this._hp = Math.max(newHP, 0);
+    this._hp = Math.min(this.hp, this.totalHp);
+  
+    // If player health is equal 0 opponent wins
+    setTimeout(() => {
+      if (this._hp === 0) {
+        if ((this.main && !stageTransition) || (!this.main && this.stage == 1)) {
+          gameOver(this.opponent);
+        } else if (this.stage == 0) {
+          stageTransition = true;
+          nextStage();
+        }
+      }
+    }, 1000);
+  
+    // Update the player hp bar
+    const barWidth = (this._hp / this.totalHp) * 100; 
+    this.hpElement.style.width = barWidth + '%';
+  }
+
+  isFrozen() {
+    return this.frozen > 0;
+  }
+
+  defrost() {
+    this.frozen--;
+  }
+
+  evolve() {
+    this.stage++;
+  }
+
+  isWeakness(attack) {
+    this.weaknesses.forEach((w) => {
+      if (w.fighter == this.type && w.attack == attack.type)
+        return 1;
+    })
+    return 0;
+  }
+
+  // Check if attacks misses
+  willAttackMiss (accuracy) {
+    if (accuracy)
+      return Math.floor(Math.random() * 100) > accuracy;
+    return 0;
+  }
+
+  attack(attack) {
+    // 0: return false if attack misses
+    // 1: otherwise update opponents health and return true
+    
+    // if attack misses, end turn
+    if (this.willAttackMiss(attack.accuracy))
+      return 0;
+    if (this.willAttackMiss(attack.freezingAccuracy))
+      this.opponent.freeze = 2;
+    
+    if (attack.type == 'healing')
+      this.updateHp(this.hp + attack.healing);
+    else {
+      // check if attack type is opponent's weakness
+      let { type: opponentType, stage: opponentStage } = this.opponent,
+          multiplyer = 1 + (this.weaknessIndex * this.isWeakness(attack));
+      
+      // update opponent's health
+      this.opponent.updateHp(this.opponent.hp - attack.power * multiplyer);
+    }
+    return 1;
+  }
+
+  appendAttack(callable, stats) {
+    this.attacks[callable] = stats;
+  }
+}
 
 const turnText = document.getElementById('text');
 let isTurnHappening = false;
 let stageTransition = false;
-
-const playerInfo = {
-  name: 'Simões',
-  type: ['human', 'assembly'],
-  stage: 0,
-  frozen: 0
-}
-
-const opponentInfo = {
-  name: 'Mello',
-  type: ['human', 'gcc'],
-  stage: 0,
-  frozen: 0
-}
 
 const playerAttacks = {
   arduinoFlamejante: {
@@ -79,63 +198,33 @@ const opponentAttacks = {
   }
 }
 
+const playerWeaknesses = [
+  {
+    fighter: 'human',
+    attack: 'electric'
+  }
+]
+
+const opponentWeaknesses = [
+  {
+    fighter: 'assembly',
+    attack: 'pointer'
+  }
+]
+
+let player = new Fighter('Simões', ['human', 'assembly'], 280, document.getElementById('player-health'), playerAttacks, playerWeaknesses, 0.25, true);
+let opponent = new Fighter('Mello', ['human', 'gcc'], 380, document.getElementById('opponent-health'), opponentAttacks, opponentWeaknesses, 0.075, false);
+
 function gameOver (winner) {
   // Wait 1000 (Health loss animation)
   setTimeout(() => {
     // Update HTML text with the winner
-    turnText.innerText = winner + ' vence!';
+    turnText.innerText = winner.name + ' vence!';
     // Open alert with the winner
-    alert(winner + ' é o vencedor! Feche este alerta para jogar novamente');
+    alert(winner.name + ' é o vencedor! Feche este alerta para jogar novamente');
     // Reload the game
     window.location.reload();
   }, 1000);
-}
-
-// Check if attacks misses
-function willAttackMiss (accuracy) {
-  if (accuracy)
-    return Math.floor(Math.random() * 100) > accuracy;
-  return 0;
-}
-
-function updatePlayerHp(newHP) {
-  // Prevents the HP to go off boundaries
-  playerHp = Math.max(newHP, 0);
-  playerHp = Math.min(playerHp, playerTotalHp);
-
-  // If player health is equal 0 opponent wins
-  setTimeout(() => {
-    if (playerHp === 0 && !stageTransition) {
-      gameOver('Mello');
-    }
-  }, 1000);
-
-    // Update the player hp bar
-  const barWidth = (playerHp / playerTotalHp) * 100;
-  playerHpElement.style.width = barWidth + '%';
-  
-}
-
-function updateOpponentHp(newHP) {
-  // Prevents the HP to go off boundaries
-  console.log(opponentHp)
-  opponentHp = Math.max(newHP, 0);
-  opponentHp = Math.min(opponentHp, opponentTotalHp);
-  console.log(opponentHp)
-
-  // If oppont health is equal 0 player wins
-  if (opponentHp === 0) {
-    if (playerInfo.stage == 1)
-      gameOver('Simões')
-    else {
-      stageTransition = true;
-      nextStage()
-    }
-  }
-
-  // Update the opponents hp bar
-  const barWidth = (opponentHp / opponentTotalHp) * 100;
-  opponentHpElement.style.width = barWidth + '%';
 }
 
 function nextStage () {
@@ -146,132 +235,83 @@ function nextStage () {
     document.getElementById("fight").style.backgroundImage = "url('/assets/icmc_evolved.jpg')"
 
     setTimeout(() => {
-      playerInfo['stage'] = 1
-      opponentInfo['stage'] = 1
+      player.evolve();
+      opponent.evolve();
 
-      playerTotalHp *= 2
-      updatePlayerHp(playerTotalHp);
-      opponentTotalHp *= 2.75
-      updateOpponentHp(opponentTotalHp);
+      player.totalHp = player.totalHp * 2;
+      player.updateHp(player.totalHp);
+      opponent.totalHp = opponent.totalHp * 2.75;
+      opponent.updateHp(opponent.totalHp);
 
-      playerAttacks['restauracaoSistematica'] = { power: 0, accuracy: 65, healing: 80, name: 'Restauração Sistemática', type: 'healing' }
-      playerAttacks['fonteOnipotente'] = { power: 220, accuracy: 35, name: 'Fonte Onipotente', type: 'electric'}
-      opponentAttacks['curaValgrindiana'] = { power: 0, accuracy: 50, healing: 70, name: 'Cura Valgrindiana', type: 'healing'}
-      opponentAttacks['sudoKillall'] = { power: 220, accuracy: 30, name: 'sudo killall', type: 'purge'}
+      player.appendAttack('restauracaoSistematica', { power: 0, accuracy: 65, healing: 80, name: 'Restauração Sistemática', type: 'healing' })
+      player.appendAttack('fonteOnipotente', { power: 220, accuracy: 35, name: 'Fonte Onipotente', type: 'electric'})
+      opponent.appendAttack('curaValgrindiana', { power: 0, accuracy: 50, healing: 70, name: 'Cura Valgrindiana', type: 'healing'})
+      opponent.appendAttack('sudoKillall', { power: 220, accuracy: 30, name: 'sudo killall', type: 'purge'})
 
       document.getElementsByClassName("row-evolve")[0].style.display = "flex";
     }, 1000)
     stageTransition = false;
-  }, 1000)
-}
-
-function isWeakness(characterType, attackType) {
-  // 0: return false if attack type is not character's weakness
-  // 1: return true if it is character's weakness
-  if (characterType == "human" && attackType == "electric")
-    return 1;
-  if (characterType == "assembly" && attackType == "pointer")
-    return 1;
-  return 0;
-}
-
-// player attack function that receives the used attack
-function playerAttack(attack) {
-  // 0: return false if attack misses
-  // 1: otherwise update opponents health and return true
-  
-  // if attack misses, end turn
-  if (willAttackMiss(attack.accuracy))
-    return 0;
-  if (willAttackMiss(attack.freezingAccuracy))
-    opponentInfo['frozen'] = 2;
-
-  if (attack.type == 'healing')
-    updatePlayerHp(playerHp + attack.healing);
-  else {
-    // check if attack type is opponent's weakness
-    let { type: opponentType, stage: opponentStage } = opponentInfo,
-        weaknessIndex = 0.25,
-        multiplyer = 1 + (weaknessIndex * isWeakness(opponentType[opponentStage], attack.type));
-    
-    // update opponent's health
-    updateOpponentHp(opponentHp - attack.power * multiplyer);
-    console.log(`O ataque do jogador tirou ${attack.power * multiplyer} de HP`)
-  }
-  return 1;
-}
-
-// opponent attack function that receives the used attack
-function opponentAttack(attack) {
-  // 0: return false if attack misses
-  // 1: otherwise update player health and return true
-
-  // if attack misses, end turn
-  if (willAttackMiss(attack.accuracy))
-    return 0;
-
-  if (attack.type == 'healing')
-    updateOpponentHp(opponentHp + attack.healing)
-  else {
-    // check if attack type is player's weakness
-    let { type: playerType, stage: playerStage } = playerInfo,
-        weaknessIndex = 0.075,
-        multiplyer = 1 + (weaknessIndex * isWeakness(playerType[playerStage], attack.type));
-    
-    // update player's health
-    updatePlayerHp(playerHp - attack.power * multiplyer);
-    console.log(`O ataque do oponente tirou ${attack.power * multiplyer} de HP`)
-  }
-  return 1;
+  }, 1000);
 }
 
 function chooseOpponentAttack () {
   // Put all opponents attacks in a array
-  const possibleAttacks = Object.values(opponentAttacks);
+  const possibleAttacks = Object.values(opponent.attacks);
 
   // Randomly chooses one attack from the array
   return possibleAttacks[Math.floor(Math.random() * possibleAttacks.length)];
 }
 
 function turn(playerChosenAttack) {
+  player.versus = opponent;
+  opponent.versus = player;
+
   // Don't start another turn till the current one is not finished
   if (isTurnHappening) {
     return;
   }
   isTurnHappening = true;
 
-  const didPlayerHit = playerAttack(playerChosenAttack);
+  const didPlayerHit = player.attack(playerChosenAttack);
 
   // Update HTML text with the used attack
-  turnText.innerText = 'Simões usou ' + playerChosenAttack.name;
+  turnText.innerText = player.name + ' usou ' + playerChosenAttack.name;
 
-  if (opponentInfo.frozen == 2)
-    turnText.innerText += ' e congelou Mello por 2 rodadas!'
+  if (opponent.frozenRounds == 2)
+    turnText.innerText += ' e congelou ' + opponent.name + ' por 2 rodadas!'
 
   // Update HTML text in case the attack misses
   if (!didPlayerHit) {
     turnText.innerText += ', mas errou!';
   }
 
+  // setTimeout(() => {
+  //   if (opponent.hp == 0) {
+    
+  //     turnText.innerText = 'Escolha um ataque';
+  //     isTurnHappening = false;
+  //     return;
+  //   }
+  // }, 2000);
   // Wait 2000ms to execute opponent attack (Player attack animation time)
-  let opponentTurn = (opponentInfo.frozen > 0) ? 0 : 2000;
+  let opponentTurn = opponent.isFrozen() ? 0 : 2500;
 
   setTimeout(() => {
     // Randomly chooses opponents attack
     const opponentChosenAttack = chooseOpponentAttack();
 
-    if (opponentInfo.frozen == 0) {
-      const didOpponentHit = opponentAttack(opponentChosenAttack);
+    if (!opponent.isFrozen()) {
+      const didOpponentHit = opponent.attack(opponentChosenAttack);
 
       // Update HTML text with the used attack
-      turnText.innerText = 'Mello usou ' + opponentChosenAttack.name;
+      turnText.innerText = opponent.name + ' usou ' + opponentChosenAttack.name;
 
       // Update HTML text in case the attack misses
       if (!didOpponentHit) {
         turnText.innerText += ', mas errou!';
       }
     } else
-      opponentInfo['frozen']--;
+      opponent.defrost();
 
     // Wait 2000ms to end the turn (Opponent attack animation time)    
     setTimeout(() => {
