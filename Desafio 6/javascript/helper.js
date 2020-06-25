e5f4472cd02754489f08bf28832876a2a9bbaff6
gabrielrspupo <gabrielrspupo@gmail.com>
2020-06-25 17:14:41 -0300

    [06] First and first bonus challenges solved

---
+++ Desafio 6/javascript/helper.js
@@ -0,0 +1,21 @@
+const HELP_TEXT = "/jokempo - Jogue uma partida de jokempô comigo!"
+
+/**
+* Sends a help message to the user with all the bot commands.
+*
+* @argument { import('node-telegram-bot-api') } bot
+* @argument { number } chatId
+* @returns { boolean } A flag to indicate whether the message was used or not.
+*/
+function main (bot, chatId, message) {
+	if (message === '/help') {
+		bot.sendMessage(chatId, HELP_TEXT);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+module.exports = {
+	main
+}
--- Desafio 6/javascript/index.js
+++ Desafio 6/javascript/index.js
@@ -2,9 +2,8 @@ process.env.NTBA_FIX_319 = true; // Silences an annoying error message.
 const TelegramBot = require('node-telegram-bot-api');
 const jokempo = require('./jokempo');
 const randomPhrases = require('./random-phrases');
-
-// replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR ACCESS TOKEN HERE';
+const helper = require('./helper');
+const token = require('./secret');
 
 if (token === 'YOUR ACCESS TOKEN HERE') {
 	console.log('You forgot to replate the access token!!! Properly read the README before continuing >:(');
@@ -22,6 +21,8 @@ bot.on('message', async (msg) => {
 	if (chatMessage.startsWith('ola') || chatMessage.startsWith('oi')) {
 		bot.sendMessage(chatId, 'Olá! Como vai o seu dia?');
 	} else if (jokempo.main(bot, chatId, chatMessage)) {
+		return;
+	} else if (helper.main(bot, chatId, chatMessage)) {
 		return;
 	} else {
 		randomPhrases.writeRandomPhrase(bot, chatId);
