6ab2dd87cb3dfe287162b41b6964f7d563370a11
gabrielrspupo <gabrielrspupo@gmail.com>
2020-07-15 23:50:51 -0300

    [07] Third bonus challenge solved

--- Desafio 7/Back-end/index.js
+++ Desafio 7/Back-end/index.js
@@ -3,12 +3,20 @@ const process = require('process');
 const cors = require('cors');
 const fs = require('fs');
 
+const adjectives = require('./adjectives');
+const animals = require('./animals');
+const colors = require('./colors');
+
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9090;
 
 const messages = [];
+const usedNames = [];
+const usedColors = [];
 
 backupMessages();
+backupUsers();
+
 app.use(cors());
 app.use(express.json());
 
@@ -23,6 +31,32 @@ app.post('/messages', (req, res) => {
 	saveMessage(msg);
 	res.send({});
 });
+
+app.get('/user', (req, res) => {
+	/** @argument { any[] } arr */
+	function randomItemFromArray (arr) {
+		return arr[Math.floor(Math.random() * arr.length)];
+	}
+
+	let name = ''; let color = '';
+
+	do {
+		name = `${randomItemFromArray(adjectives)} ${randomItemFromArray(animals)}`;
+		color = randomItemFromArray(colors);
+	} while (usedNames.includes(name) && usedColors.includes(color));
+	res.send({ name, color });
+})
+
+app.post('/user', (req, res) => {
+	const { name, color } = req.body;
+
+	if (usedNames.includes(name) && usedColors.includes(color)) res.sendStatus(500);
+	else {
+		usedNames.push(name); usedColors.push(color);
+		saveUser(req.body);
+		res.send({});
+	}
+})
 
 /**
  * Backup all previous messages from a backup file.
@@ -42,6 +76,25 @@ function backupMessages() {
 		}
 	})
 }
+
+function backupUsers() {
+	fs.readFile("backup-users.txt", "utf-8", (err, data) => {
+		if (err) console.log('Unable to backup users! Maybe the backup file is empty...');
+
+		else {
+			// Insert all messages into an array
+			const backup = data.split('\n');
+			const backupSize = backup.length - 1;
+
+			for (let u = 0; u < backupSize; ++ u) {
+				// Parse message into a JSON object
+				let user = JSON.parse(backup[u]);
+				usedNames.push(user.name);
+				usedColors.push(user.color);
+			}
+		}
+	})
+}
 /**
  * Append a JSON-formatted message to the backup file.
  * @param  { Object } msg
@@ -49,6 +102,12 @@ function backupMessages() {
 function saveMessage(msg) {
 	fs.appendFile("backup.txt", JSON.stringify(msg)+'\n', "utf-8", err => {
 		if (err) console.log("Unable to save message!", err);
+	})
+}
+
+function saveUser(user) {
+	fs.appendFile("backup-users.txt", JSON.stringify(user)+'\n', "utf-8", err => {
+		if (err) console.log("Unable to save user!", err);
 	})
 }
 
